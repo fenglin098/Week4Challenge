b4e5a604aa78f289db0312be6159facc814a8087
fenglin098 <feng.lin098@gmail.com>
2019-11-14 15:01:14 -0500

    draft

--- src/Main.java
+++ src/Main.java
@@ -55,136 +55,145 @@ public class Main {
             case "n":
                 break;
         }
-        System.out.println("Options:\n1 = Add Student\n2 = Add Faculty\n3 = Edit Student\n4 = Edit Faculty\n5 = Add Class\n6 = Edit Class\n7 = Enroll Student\n8 = Hire a Faculty\n9 = View All Information\nEnter \"q\" to quit");
-        int answerOption = sc.nextInt();
-        switch (answerOption) {
-            case 1:
-                System.out.println("Please enter student name: ");
-                String inputSN = sc.next();
-                inputSN += sc.nextLine();
-                student.setName(inputSN);
-                System.out.println("Please enter student email: ");
-                String inputSE = sc.next();
-                student.setEmail(inputSE);
-                System.out.println("Please enter student ID");
-                int inputSID = sc.nextInt();
-                student.setId(inputSID);
-                arrStudent.add(student);
-                break;
-            case 2:
-                System.out.println("Please enter faculty name: ");
-                String inputFN = sc.next();
-                inputFN += sc.nextLine();
-                faculty.setName(inputFN);
-                System.out.println("Please enter faculty email: ");
-                String inputFE = sc.next();
-                faculty.setEmail(inputFE);
-                System.out.println("Please enter faculty ID");
-                int inputFID = sc.nextInt();
-                faculty.setId(inputFID);
-                arrFaculty.add(faculty);
-                break;
-            case 3:
-                System.out.println("Please enter the ID of the student you would like to edit: ");
-                int editSID = sc.nextInt();
-                System.out.println("What is the new name of the student?");
-                String editSN = sc.next();
-                editSN += sc.nextLine();
-                for (Student s : arrStudent) {
-                    if (s.getId() == editSID) {
-                        s.setName(editSN);
+        String answerOption2;
+        do {
+            System.out.println("Options:\n1 = Add Student\n2 = Add Faculty\n3 = Edit Student\n4 = Edit Faculty\n5 = Add Class\n6 = Edit Class\n7 = Enroll Student\n8 = Hire a Faculty\n9 = View All Information\nEnter \"q\" to quit");
+            int answerOption = sc.nextInt();
+            switch (answerOption) {
+                case 1:
+                    System.out.println("Please enter student name: ");
+                    String inputSN = sc.next();
+                    inputSN += sc.nextLine();
+                    student.setName(inputSN);
+                    System.out.println("Please enter student email: ");
+                    String inputSE = sc.next();
+                    student.setEmail(inputSE);
+                    System.out.println("Please enter student ID");
+                    int inputSID = sc.nextInt();
+                    student.setId(inputSID);
+                    arrStudent.add(student);
+                    break;
+                case 2:
+                    System.out.println("Please enter faculty name: ");
+                    String inputFN = sc.next();
+                    inputFN += sc.nextLine();
+                    faculty.setName(inputFN);
+                    System.out.println("Please enter faculty email: ");
+                    String inputFE = sc.next();
+                    faculty.setEmail(inputFE);
+                    System.out.println("Please enter faculty ID");
+                    int inputFID = sc.nextInt();
+                    faculty.setId(inputFID);
+                    arrFaculty.add(faculty);
+                    break;
+                case 3:
+                    System.out.println("Please enter the ID of the student you would like to edit: ");
+                    int editSID = sc.nextInt();
+                    System.out.println("What is the new name of the student?");
+                    String editSN = sc.next();
+                    editSN += sc.nextLine();
+                    for (Student s : arrStudent) {
+                        if (s.getId() == editSID) {
+                            s.setName(editSN);
+                        }
                     }
-                }
-                break;
-            case 4:
-                System.out.println("Please enter the ID of the faculty you would like to edit: ");
-                int editFID = sc.nextInt();
-                System.out.println("What is the new name of the faculty?");
-                String editFN = sc.next();
-                editFN += sc.nextLine();
-                for (Faculty f : arrFaculty) {
-                    if (f.getId() == editFID) {
-                        f.setName(editFN);
+                    break;
+                case 4:
+                    System.out.println("Please enter the ID of the faculty you would like to edit: ");
+                    int editFID = sc.nextInt();
+                    System.out.println("What is the new name of the faculty?");
+                    String editFN = sc.next();
+                    editFN += sc.nextLine();
+                    for (Faculty f : arrFaculty) {
+                        if (f.getId() == editFID) {
+                            f.setName(editFN);
+                        }
                     }
-                }
-                break;
-            case 5:
-                System.out.println("Please enter the new course title: ");
-                String inputCTitle = sc.next();
-                inputCTitle += sc.nextLine();
-                course.setCourseTitle(inputCTitle);
-                System.out.println("Please enter the new course ID: ");
-                int inputCID = sc.nextInt();
-                course.setCourseID(inputCID);
-                System.out.println("Please enter the new course description: ");
-                String inputCDescription = sc.next();
-                inputCDescription += sc.nextLine();
-                course.setDescription(inputCDescription);
-                arrCourse.add(course);
-                break;
-            case 6:
-                System.out.println("Please enter the ID of the course you would like to edit: ");
-                int editCID = sc.nextInt();
-                System.out.println("What is the new title of the new course?");
-                String editCTitle = sc.next();
-                editCTitle+=sc.nextLine();
-                for (Class c: arrCourse ){
-                    if (c.getCourseID()==editCID){
-                        c.setCourseTitle(editCTitle);
+                    break;
+                case 5:
+                    System.out.println("Please enter the new course title: ");
+                    String inputCTitle = sc.next();
+                    inputCTitle += sc.nextLine();
+                    course.setCourseTitle(inputCTitle);
+                    System.out.println("Please enter the new course ID: ");
+                    int inputCID = sc.nextInt();
+                    course.setCourseID(inputCID);
+                    System.out.println("Please enter the new course description: ");
+                    String inputCDescription = sc.next();
+                    inputCDescription += sc.nextLine();
+                    course.setDescription(inputCDescription);
+                    arrCourse.add(course);
+                    break;
+                case 6:
+                    System.out.println("Please enter the ID of the course you would like to edit: ");
+                    int editCID = sc.nextInt();
+                    System.out.println("What is the new title of the new course?");
+                    String editCTitle = sc.next();
+                    editCTitle += sc.nextLine();
+                    for (Class c : arrCourse) {
+                        if (c.getCourseID() == editCID) {
+                            c.setCourseTitle(editCTitle);
+                        }
                     }
-                }
-                break;
-            case 7:
-                System.out.println("Please enter the Course ID: ");
-                int enrollmentCSID = sc.nextInt();
-                System.out.println("Please enter the Student ID: ");
-                int enrollmentSID = sc.nextInt();
-                System.out.println("Please enter the enrollment date: ");
-                String enrollmentDate = sc.next();
-                for (Enrollment e: arrEnrollment){
-                    if (e.getEnrolledCID()==enrollmentCSID){
-                        e.setEnrolledSID(enrollmentSID);
-                        e.setEnrolledDate(enrollmentDate);
+                    break;
+                case 7:
+                    System.out.println("Please enter the Course ID: ");
+                    int enrollmentCSID = sc.nextInt();
+                    System.out.println("Please enter the Student ID: ");
+                    int enrollmentSID = sc.nextInt();
+                    System.out.println("Please enter the enrollment date: ");
+                    String enrollmentDate = sc.next();
+                    for (Enrollment e : arrEnrollment) {
+                        if (e.getEnrolledCID() == enrollmentCSID) {
+                            e.setEnrolledSID(enrollmentSID);
+                            e.setEnrolledDate(enrollmentDate);
+                        }
                     }
-                }
-                break;
-            case 8:
-                System.out.println("Please enter the Course ID: ");
-                int enrollmentCFID = sc.nextInt();
-                System.out.println("Please enter the Faculty ID: ");
-                int enrollmentFID = sc.nextInt();
-                System.out.println("Please enter the enrollment date: ");
-                String enrollmentDateF = sc.next();
-                for (Enrollment e: arrEnrollment){
-                    if (e.getEnrolledCID()==enrollmentCFID){
-                        e.setEnrolledFID(enrollmentFID);
-                        e.setEnrolledDate(enrollmentDateF);
+                    break;
+                case 8:
+                    System.out.println("Please enter the Course ID: ");
+                    int enrollmentCFID = sc.nextInt();
+                    System.out.println("Please enter the Faculty ID: ");
+                    int enrollmentFID = sc.nextInt();
+                    System.out.println("Please enter the enrollment date: ");
+                    String enrollmentDateF = sc.next();
+                    for (Enrollment e : arrEnrollment) {
+                        if (e.getEnrolledCID() == enrollmentCFID) {
+                            e.setEnrolledFID(enrollmentFID);
+                            e.setEnrolledDate(enrollmentDateF);
+                        }
                     }
-                }
-                break;
-            case 9:
-                System.out.println("All Students:\n");
-                for (Student s: arrStudent){
-                    System.out.println("Student ID: "+s.getId());
-                    System.out.println("Student name: "+s.getName());
-                }
+                    break;
+                case 9:
+                    System.out.println("All Students:\n");
+                    for (Student s : arrStudent) {
+                        System.out.println("Student ID: " + s.getId());
+                        System.out.println("Student name: " + s.getName());
+                        System.out.println();
+                    }
 
-                System.out.println("All Faculty:\n");
-                for (Faculty f: arrFaculty){
-                    System.out.println("Faculty iD: "+f.getId());
-                    System.out.println("Faculty name: "+f.getName());
-                }
+                    System.out.println("All Faculty:\n");
+                    for (Faculty f : arrFaculty) {
+                        System.out.println("Faculty iD: " + f.getId());
+                        System.out.println("Faculty name: " + f.getName());
+                        System.out.println();
+                    }
 
-                System.out.println("All Classes:\n");
-                for (Class c: arrCourse){
-                    System.out.println("Class ID: "+c.getCourseID());
-                    System.out.println("Class name: "+c.getCourseTitle());
-                    System.out.println("Class description: "+c.getDescription());
-                }
-                 break;
-        }
+                    System.out.println("All Classes:\n");
+                    for (Class c : arrCourse) {
+                        System.out.println("Class ID: " + c.getCourseID());
+                        System.out.println("Class name: " + c.getCourseTitle());
+                        System.out.println("Class description: " + c.getDescription());
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    System.out.println("Goodbye");
+            }
+            System.out.println("Would you like to select another option? (Y/N)");
+            answerOption2 = sc.next().toLowerCase();
+        } while (answerOption2.equalsIgnoreCase("y"));
     }
-
 
     public static void loginCheck() {
         boolean IsValid = false;
@@ -195,7 +204,6 @@ public class Main {
                 System.out.println("Please enter your password: ");
                 String loginPassword = sc.next();
                 if (l.getEmail().equals(loginEmail)) {
-
                     Login login = l;
                     if (l.getPassword().equals(loginPassword)) {
                         IsValid = true;
